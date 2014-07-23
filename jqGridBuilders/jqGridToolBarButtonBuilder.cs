e8dc24e4546dc3de58b0ef53ad4ab2ab2ce160d6
cpistiner <chpistiner@gmail.com>
2014-07-23 12:05:08 -0300

    agrega la primer versión de jqGridToolBarButtonBuilder
    primer versión de jqGridToolBarButtonBuilder - herramienta para crear
    botones en la toolbar del jqGridModel

---
+++ jqGridBuilders/jqGridToolBarButtonBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Trirand.Web.Mvc;
+
+namespace Web.ViewModels.JQGridBuilders
+{
+	public class JQGridToolBarButtonBuilder
+	{
+		internal static JQGridToolBarButton NonDefaultEdit()
+		{
+			var button = new JQGridToolBarButton();
+			button.ButtonIcon = "ui-icon-pencil";
+			button.ToolTip = "Modificar fila seleccionada (Ctrl+M)";
+			button.Text = " ";
+			button.Position = ToolBarButtonPosition.First;
+			button.OnClick = "modificarFila";
+
+			return button;
+		}
+
+		internal static JQGridToolBarButton NonDefaultAdd()
+		{
+			var button = new JQGridToolBarButton();
+			button.ButtonIcon = "ui-icon-plus";
+			button.ToolTip = "Agregar nueva fila (Ctrl+N)";
+			button.Text = " ";
+			button.Position = ToolBarButtonPosition.First;
+			button.OnClick = "agregarFila";
+
+			return button;
+		}
+		
+		internal static JQGridToolBarButton NonDefaultViewRow()
+		{
+			var button = new JQGridToolBarButton();
+			button.ButtonIcon = "ui-icon-document";
+			button.ToolTip = "Ver fila";
+			button.Text = " ";
+			button.Position = ToolBarButtonPosition.First;
+			button.OnClick = "verFila";
+
+			return button;
+		}
+
+		internal static JQGridToolBarButton ColumnChooser()
+		{
+			var button = new JQGridToolBarButton();
+			button.ButtonIcon = "ui-icon-grip-dotted-vertical";
+			button.ToolTip = "Mostrar/ocultar columnas";
+			button.Text = " ";
+			button.Position = ToolBarButtonPosition.Last;
+			button.OnClick = "seleccionarColumnas";
+
+			return button;
+		}
+	}
+}
