00afb65424792370a02fa62dfd97ae74852c312b
César Pistiner <chpistiner@gmail.com>
2015-02-13 00:52:15 -0300

    Creates test proyect for JQGridUtillities and first test for jqGridToolBarButtonBuilder

---
+++ JQGridUtillities.Test/jqGridBuilder/jqGridToolBarButtonBuilderTest.cs
@@ -0,0 +1,26 @@
+﻿using JQGridUtilities.JQGridBuilders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace JQGridUtillities.Test.jqGridBuilder
+{
+	[TestClass]
+	public class jqGridToolBarButtonBuilderTest
+	{
+		[TestMethod]
+		public void NonDefaultEdit_SetTooltipByDefault()
+		{
+			var button = JQGridToolBarButtonBuilder.NonDefaultEdit();
+
+			Assert.AreEqual("Edit selected row", button.ToolTip);
+		}
+
+		[TestMethod]
+		public void NonDefaultEdit_TooltipInEnglishByDefault()
+		{
+			var button = JQGridToolBarButtonBuilder.NonDefaultEdit();
+
+			Assert.AreEqual("Edit selected row", button.ToolTip);
+		}
+	}
+}
